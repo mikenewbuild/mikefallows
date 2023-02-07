bf6edb8412976ed396883a65a6b4ea607fa758a9
Mike Fallows <43390704+mikenewbuild@users.noreply.github.com>
2023-02-07 13:36:18 +0000

    Add search and update node to v16 (#13)
    * Update profile
    
    * Install pagefind
    
    * Build index for pagefind search
    
    * Remove redundant assignment
    
    * Add search page
    
    * Add search link to footer
    
    * remove Twitter link
    
    * Improve layout for search index
    
    * Update date in footer
    
    * Escape cli command for netlify build
    
    * Remove excluding `code` from search index
    
    Won't work with netlify build
    
    * Remove npx from pagefind command
    
    * Disable pagefind to test build
    
    * Update pagefind script
    
    * Update .nvmrc v16

--- .eleventy.js
+++ .eleventy.js
@@ -153,8 +153,8 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setLibrary('md', markdownLibrary);
 
   // Build index for pagefind search
-  eleventyConfig.on('eleventy.after', () => {
-    execSync(`npx pagefind --source _site --glob \"**/*.html\" --exclude-selectors "code"`, { encoding: 'utf-8' })
+  eleventyConfig.on('eleventy.after', async () => {
+    execSync(`pagefind --source ./_site`, { encoding: 'utf-8' })
   })
 
   // Override Browsersync defaults (used only with --serve)
